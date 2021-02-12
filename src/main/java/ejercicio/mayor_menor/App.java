4594e7e213e94374339bc4d6a7ce4e192135be1d
danielburgosr96 <danielburgosr96@gmail.com>
2021-02-12 15:08:41 -0500

    Ejercicios uno y dos

--- src/main/java/ejercicio/mayor_menor/App.java
+++ src/main/java/ejercicio/mayor_menor/App.java
@@ -1,8 +1,25 @@
 package ejercicio.mayor_menor;
 
 public class App {
+    private static void primerEjercicio(){
+        System.out.println("Primer Ejercicio: valores quemados");
+        Mayor primerPunto = new Mayor(4, 34);
+        primerPunto.compararNumeros();
+    }
+
+    private static void segundoEjercicio(){
+        System.out.println("Segundo Ejercicio: valores por consola");
+        Mayor segundoPunto = new Mayor(Interaccion.pedirNumero("Primer numero"),
+                Interaccion.pedirNumero("Segundo numero"));
+        segundoPunto.compararNumeros();
+    }
+
     public static void main(String[] args) {
-        Mayor prueba = new Mayor(4, 34);
-        prueba.compararNumeros();
+
+        App.primerEjercicio();
+
+        System.out.println("***************************************");
+
+        App.segundoEjercicio();
     }
 }
---
+++ src/main/java/ejercicio/mayor_menor/Interaccion.java
@@ -0,0 +1,28 @@
+package ejercicio.mayor_menor;
+
+import java.util.Scanner;
+import java.util.concurrent.locks.ReadWriteLock;
+
+public class Interaccion {
+    //Declarar variables
+
+    private Mayor numeros;
+
+
+    public static int pedirNumero(String numeroS){
+        int numero = 0;
+        boolean validateInput = false;
+        do{
+            try{
+                System.out.println("Por favor ingrese el " + numeroS);
+                Scanner in = new Scanner(System.in);
+                numero = in.nextInt();
+                validateInput = true;
+            }catch (Exception e){
+                System.out.println("El dato ingresado debe ser un numero entero");
+                System.out.println("----------------------------");
+            }
+        }while (!validateInput);
+        return numero;
+    }
+}
