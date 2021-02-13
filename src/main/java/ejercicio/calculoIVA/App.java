61054f7c9b5bc64dcc7822337de45eaef8cb4346
danielburgosr96 <danielburgosr96@gmail.com>
2021-02-12 19:36:52 -0500

    Ejercicio calculo IVA

--- src/main/java/ejercicio/calculoIVA/App.java
+++ src/main/java/ejercicio/calculoIVA/App.java
@@ -1,2 +1,19 @@
-package ejercicio.calculoIVA;public class App {
+package ejercicio.calculoIVA;
+
+import ejercicio.helpers.InputsNumbers;
+
+import java.util.logging.SocketHandler;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Ingrese el precio del producto");
+
+        Calculadora calculadora = new Calculadora(InputsNumbers.pedirDoublePositivo("El precio debe" +
+                " ser mayor a O"));
+
+        System.out.println("El precio final de su producto es: " + calculadora.calcularPrecioFinal());
+
+    }
+
+
 }
--- src/main/java/ejercicio/calculoIVA/Calculadora.java
+++ src/main/java/ejercicio/calculoIVA/Calculadora.java
@@ -1,2 +1,23 @@
-package ejercicio.calculoIVA;public class Calculadora {
+package ejercicio.calculoIVA;
+
+public class Calculadora {
+    private double precio;
+
+    public Calculadora(double precio){
+        this.precio = precio;
+    }
+
+    public double getPrecio(){
+        return this.precio;
+    }
+
+    public void setPrecio(){
+        this.precio = precio;
+    }
+
+    public double calcularPrecioFinal(){
+
+        double precioFinal = getPrecio()*1.21;
+        return precioFinal;
+    }
 }
