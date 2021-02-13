5d1b27b05605e193f6560fdf2d6d8a27960afcf7
danielburgosr96 <danielburgosr96@gmail.com>
2021-02-12 19:39:16 -0500

    Creacion de paquete helpers

--- src/main/java/ejercicio/helpers/InputsNumbers.java
+++ src/main/java/ejercicio/helpers/InputsNumbers.java
@@ -2,24 +2,62 @@ package ejercicio.helpers;
 
 import java.util.Scanner;
 
-public class Inputs {
+public class InputsNumbers {
 
     /**
-     * Pide un entero al usuario por consola
-     * @return El numero que el usuario metio. null si el input del usuario no es valido.
+     * Metodo para pedirle un numero entero al usuario. El metodo continua en el bucle hasta que
+     * el input del usuario sea valido.
+     * @return El numero que dado por el usuario.
      */
-    public static int PedirInt(){
+    public static int pedirInt(){
+        int numero = 0;
+        boolean validador = false;
+        do {
+            try{
+                Scanner in = new Scanner(System.in);
+                numero = in.nextInt();
+                validador = true;
+            }catch (Exception e){
+                System.out.println("El dato ingresado debe ser un numero entero");
+                System.out.println("Por favor intente de nuevo");
+            }
+        }while(!validador);
 
-        Integer numero;
-
-        try{
-            System.out.println("Por favor ingrese el numero");
-            Scanner in = new Scanner(System.in);
-            numero = in.nextInt();
-        }catch (Exception e){
-            numero = null;
-            System.out.println("El dato ingresado debe ser un numero entero");
-        }
         return numero;
     }
+
+    public static double pedirDouble(){
+        double numero = 0;
+        boolean validador = false;
+        do {
+            try{
+                Scanner in = new Scanner(System.in);
+                numero = Double.parseDouble(in.nextLine());
+                validador = true;
+            }catch (Exception e){
+                System.out.println("El dato ingresado debe ser un numero");
+                System.out.println("Por favor intente de nuevo");
+            }
+        }while(!validador);
+
+        return numero;
+    }
+
+    public static double pedirDoublePositivo(String mensajeError){
+        double numero;
+        boolean validador;
+        do{
+            numero = pedirDouble();
+            if(numero > 0){
+                validador = true;
+            }
+            else{
+                System.out.println(mensajeError);
+                System.out.println("Por favor intente de nuevo");
+                validador = false;
+            }
+        }while (!validador);
+        return numero;
+    }
+
 }
