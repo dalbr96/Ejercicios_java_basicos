bdfc745fa2e9f6c84b8b5e9f9b2510bdf0149fff
danielburgosr96 <danielburgosr96@gmail.com>
2021-02-13 20:09:54 -0500

    Ejercicio 16. Aplicacion masa corporal personas

--- src/main/java/ejercicio/persona/App.java
+++ src/main/java/ejercicio/persona/App.java
@@ -1,2 +1,94 @@
-package ejercicio.persona;public class App {
+package ejercicio.persona;
+
+
+import ejercicio.helpers.Inputs;
+
+public class App {
+    private static String nombre;
+    private static int edad;
+    private static String dni;
+    private static char sexo;
+    private static double peso;
+    private static double altura;
+
+    private static void pedirDatos(){
+        System.out.println("Por favor ingresa tu nombre");
+        nombre = Inputs.pedirString();
+        System.out.println("Por favor ingresa tu edad");
+        edad = Inputs.pedirIntPositivo("La edad debe ser positiva");
+        System.out.println("Por favor ingresa tu sexo. H - Hombre, M - Mujer");
+        sexo = Inputs.pedirChar();
+        System.out.println("Por favor ingresa tu altura en metros");
+        altura = Inputs.pedirDoublePositivo("La altura debe ser positiva");
+        System.out.println("Por favor ingresa tu peso en kilogramos");
+        peso = Inputs.pedirDoublePositivo("El peso debe ser positivo");
+    }
+
+    private static void nombrarPersona(Persona persona){
+        persona.setNombre("Pedro Perez");
+        persona.setEdad(12);
+        persona.setSexo('H');
+        persona.setAltura(1.42d);
+        persona.setPeso(40d);
+    }
+
+    private static void calcularPesoIdeal(Persona persona){
+            switch (persona.calcularIMC()) {
+                case -1:
+                    System.out.println("Está debajo de su peso ideal");
+                    break;
+                case 0:
+                    System.out.println("Está en su peso ideal");
+                    break;
+                case 1:
+                    System.out.println("Está por encima de su peso ideal");
+                    break;
+            }
+
+    }
+
+    private static void imprimirMayoriaDeEdad(Persona persona){
+        if(persona.esMayorDeEdad()){
+            System.out.println("Es mayor de edad");
+        }else{
+            System.out.println("No es mayor de edad");
+        }
+    }
+
+    private static void imprimirDatos(Persona persona){
+        System.out.println(persona);
+        System.out.println("------------------------");
+    }
+
+    private static void generarImpresion(Persona[] personas){
+        for(Persona persona: personas){
+
+            String inicio = "La persona " + persona.getNombre() + " con DNI: " + persona.getDni() + " ";
+
+            System.out.println(inicio);
+
+            calcularPesoIdeal(persona);
+
+            imprimirMayoriaDeEdad(persona);
+
+            System.out.println("Y sus datos son: ");
+
+            imprimirDatos(persona);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        pedirDatos();
+
+        Persona primerPersona = new Persona(nombre, edad, sexo, peso,altura);
+        Persona segundaPersona = new Persona(nombre, edad, sexo);
+        Persona tercerPersona = new Persona();
+
+        nombrarPersona(tercerPersona);
+
+        Persona[] personas = {primerPersona, segundaPersona, tercerPersona};
+
+        generarImpresion(personas);
+    }
 }
--- src/main/java/ejercicio/persona/Persona.java
+++ src/main/java/ejercicio/persona/Persona.java
@@ -1,19 +1,20 @@
-package ejercicio.personas.objetos;
+package ejercicio.persona;
+
+import java.util.Random;
 
 public class Persona {
     public static final char SEXO_DEFAULT = 'H';
 
     private String nombre;
     private int edad;
-    private int dni;
+    private String dni;
     private char sexo;
     private double peso;
     private double altura;
 
 
-    public Persona(String nombre, int edad, int dni, char sexo, double peso, double altura){
+    public Persona(String nombre, int edad, char sexo, double peso, double altura){
         this(nombre, edad, sexo);
-        this.dni = dni;
         this.peso = peso;
         this.altura = altura;
     }
@@ -22,13 +23,13 @@ public class Persona {
         this();
         this.nombre = nombre;
         this.edad = edad;
-        this.sexo = sexo;
+        this.sexo = comprobarSexo(sexo);
     }
 
     public Persona(){
         this.nombre = "";
         this.edad = 0;
-        this.dni = 0;
+        this.dni = generaDNI();
         this.sexo = SEXO_DEFAULT;
         this.peso = 0d;
         this.altura = 0d;
@@ -50,13 +51,10 @@ public class Persona {
         this.altura = altura;
     }
 
-    public int getDni() {
+    public String getDni() {
         return dni;
     }
 
-    public void setDni(int dni) {
-        this.dni = dni;
-    }
 
     public double getPeso() {
         return peso;
@@ -84,7 +82,7 @@ public class Persona {
     }
 
     public int calcularIMC(){
-        double imc = getPeso()/Math.pow(getAltura(), 2);
+        double imc = getPeso()/Math.pow(getAltura(), 2.0);
         int returnValue ;
         if(imc<20){
             returnValue = - 1;
@@ -108,5 +106,126 @@ public class Persona {
         }
         return returnValue;
     }
+
+    private char comprobarSexo(char sexo){
+        char returnValue;
+        switch (sexo){
+            case 'M':
+                returnValue = 'M';
+                break;
+            case 'H':
+            default:
+                returnValue = 'H';
+                break;
+        }
+        return returnValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre='" + nombre + '\'' +
+                ", Edad=" + edad +
+                ", DNI=" + dni +
+                ", Sexo=" + sexo +
+                ", Peso=" + peso +
+                ", Altura=" + altura ;
+    }
+
+    private String generaDNI(){
+        int numero = generarNumeroString();
+        char letra = generarLetraDni(numero);
+        String dni = numero + "-" +letra;
+        return dni;
+    }
+
+    private int generarNumeroString(){
+
+        Random generar = new Random();
+
+        int numero = 10000000 + generar.nextInt(90000000);
+
+        return numero;
+    }
+
+    private char generarLetraDni(int numero){
+        char letra;
+        switch (numero%23){
+            case 0:
+                letra = 'T';
+                break;
+            case 1:
+                letra = 'R';
+                break;
+            case 2:
+                letra = 'W';
+                break;
+            case 3:
+                letra = 'A';
+                break;
+            case 4:
+                letra = 'G';
+                break;
+            case 5:
+                letra = 'M';
+                break;
+            case 6:
+                letra = 'Y';
+                break;
+            case 7:
+                letra = 'F';
+                break;
+            case 8:
+                letra = 'P';
+                break;
+            case 9:
+                letra = 'D';
+                break;
+            case 10:
+                letra = 'X';
+                break;
+            case 11:
+                letra = 'B';
+                break;
+            case 12:
+                letra = 'N';
+                break;
+            case 13:
+                letra = 'J';
+                break;
+            case 14:
+                letra = 'Z';
+                break;
+            case 15:
+                letra = 'S';
+                break;
+            case 16:
+                letra = 'Q';
+                break;
+            case 17:
+                letra = 'V';
+                break;
+            case 18:
+                letra = 'H';
+                break;
+            case 19:
+                letra = 'L';
+                break;
+            case 20:
+                letra = 'C';
+                break;
+            case 21:
+                letra = 'K';
+                break;
+            case 22:
+                letra = 'E';
+                break;
+            default:
+                letra = ' ';
+                break;
+        }
+        return letra;
+    }
+
+
 
 }
